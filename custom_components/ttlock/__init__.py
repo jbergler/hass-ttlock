3be848f7d556941c51a9b36f79042bc9530b985f
Jonas Bergler <jonas@bergler.name>
2023-04-08 08:58:48 +0000

    add basic webhook support
    lock / unlock event sort of work, no support for auto lock / passage
    mode yet

--- custom_components/ttlock/__init__.py
+++ custom_components/ttlock/__init__.py
@@ -1,15 +1,34 @@
 """The TTLock integration."""
 from __future__ import annotations
 
+import json
+import logging
+import secrets
+
+from aiohttp.web import Request
+
+from homeassistant.components import cloud, webhook
+from homeassistant.components.webhook import (
+    async_register as webhook_register,
+    async_unregister as webhook_unregister,
+)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.const import (
+    CONF_WEBHOOK_ID,
+    EVENT_HOMEASSISTANT_STARTED,
+    EVENT_HOMEASSISTANT_STOP,
+    Platform,
+)
+from homeassistant.core import CoreState, Event, HomeAssistant
 from homeassistant.helpers import aiohttp_client, config_entry_oauth2_flow
+from homeassistant.helpers.dispatcher import async_dispatcher_send
 
 from .api import TTLockApi
-from .const import DOMAIN, TT_API, TT_LOCKS
+from .const import CONF_WEBHOOK_URL, DOMAIN, SIGNAL_NEW_DATA, TT_API, TT_LOCKS
 
 PLATFORMS: list[Platform] = [Platform.LOCK]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -27,6 +46,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         TT_LOCKS: [],
     }
 
+    await WebhookHandler(hass, entry).setup()
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
@@ -38,3 +59,88 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+class WebhookHandler:
+    """Responsible for setting up/processing webhook data."""
+
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
+        """Init the thing."""
+        self.hass = hass
+        self.entry = entry
+
+    async def setup(self) -> None:
+        """Actually register the webhook."""
+        if self.hass.state == CoreState.running:
+            await self.register_webhook()
+        else:
+            self.hass.bus.async_listen_once(
+                EVENT_HOMEASSISTANT_STARTED, self.register_webhook
+            )
+
+    async def register_webhook(self, event: Event | None = None) -> None:
+        """Set up a webhook to receive pushed data."""
+        if CONF_WEBHOOK_ID not in self.entry.data:
+            _LOGGER.info("Webhook not found in config entry, creating new one")
+            data = {**self.entry.data, CONF_WEBHOOK_ID: secrets.token_hex()}
+            self.hass.config_entries.async_update_entry(self.entry, data=data)
+
+        if cloud.async_active_subscription(self.hass):
+            if CONF_WEBHOOK_URL not in self.entry.data:
+                try:
+                    webhook_url = await cloud.async_create_cloudhook(
+                        self.hass, self.entry.data[CONF_WEBHOOK_ID]
+                    )
+                except cloud.CloudNotConnected:
+                    webhook_url = webhook.async_generate_url(
+                        self.hass, self.entry.data[CONF_WEBHOOK_ID]
+                    )
+                else:
+                    data = {**self.entry.data, CONF_WEBHOOK_URL: webhook_url}
+                    self.hass.config_entries.async_update_entry(self.entry, data=data)
+            else:
+                webhook_url = self.entry.data[CONF_WEBHOOK_URL]
+        else:
+            webhook_url = webhook.async_generate_url(
+                self.hass, self.entry.data[CONF_WEBHOOK_ID]
+            )
+
+        _LOGGER.info(f"Webhook {webhook_url}")
+
+        # Ensure the webhook is not registered already
+        webhook_unregister(self.hass, self.entry.data[CONF_WEBHOOK_ID])
+
+        webhook_register(
+            self.hass,
+            DOMAIN,
+            "TTLock",
+            self.entry.data[CONF_WEBHOOK_ID],
+            self.handle_webhook,
+        )
+
+        self.hass.bus.async_listen_once(
+            EVENT_HOMEASSISTANT_STOP, self.unregister_webhook
+        )
+
+    async def handle_webhook(
+        self, hass: HomeAssistant, webhook_id: str, request: Request
+    ) -> None:
+        """Handle webhook callback."""
+        # lockId=7252408&notifyType=1&records=%5B%7B%22lockId%22%3A7252408%2C%22electricQuantity%22%3A93%2C%22serverDate%22%3A1680810180029%2C%22recordTypeFromLock%22%3A17%2C%22recordType%22%3A7%2C%22success%22%3A1%2C%22lockMac%22%3A%2216%3A72%3A4C%3ACC%3A01%3AC4%22%2C%22keyboardPwd%22%3A%221292195345%22%2C%22lockDate%22%3A1680810186000%2C%22username%22%3A%22Jonas%22%7D%5D&admin=jonas%40lemon.nz&lockMac=16%3A72%3A4C%3ACC%3A01%3AC4
+        # {'lockId': ['7252408'], 'notifyType': ['1'], 'records': ['[{"lockId":7252408,"electricQuantity":93,"serverDate":1680810180029,"recordTypeFromLock":17,"recordType":7,"success":1,"lockMac":"16:72:4C:CC:01:C4","keyboardPwd":"1292195345","lockDate":1680810186000,"username":"Jonas"}]'], 'admin': ['jonas@lemon.nz'], 'lockMac': ['16:72:4C:CC:01:C4']}
+        try:
+            data = await request.post()
+            if data:
+                for v in data.getall("records", []):
+                    for record in json.loads(v):
+                        async_dispatcher_send(hass, SIGNAL_NEW_DATA, record)
+            else:
+                _LOGGER.debug(f"handle_webhook, empty payload: {await request.text()}")
+        except ValueError:
+            return
+
+        _LOGGER.debug("Got webhook data: %s", data)
+
+    async def unregister_webhook(self, event: Event | None = None) -> None:
+        """Remove the webhook (before stop)."""
+        webhook_unregister(self.hass, self.entry.data[CONF_WEBHOOK_ID])
--- custom_components/ttlock/lock.py
+++ custom_components/ttlock/lock.py
@@ -10,7 +10,7 @@ import aiohttp
 from homeassistant.components.lock import LockEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_BATTERY_LEVEL, VOLUME
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -51,6 +51,51 @@ class TTLock(LockEntity, TTLockEntity[ApiLock]):
 
     _attr_code_format = None
 
+    RECORD_TYPE_UNLOCK = [
+        1,  # unlock by app
+        4,  # unlock by passcode
+        7,  # unlock by IC card
+        8,  # unlock by fingerprint
+        9,  # unlock by wrist strap
+        10,  # unlock by Mechanical key
+        12,  # unlock by gateway
+        46,  # unlock by unlock key
+        49,  # unlock by hotel card
+        50,  # unlocked due to the high temperature
+        55,  # unlock with key fob
+        57,  # unlock with QR code success
+        63,  # auto unlock at passage mode
+    ]
+
+    RECORD_TYPE_LOCK = [
+        11,  # lock by app
+        33,  # lock by fingerprint
+        34,  # lock by passcode
+        35,  # lock by IC card
+        36,  # lock by Mechanical key
+        45,  # Auto Lock
+        47,  # lock by lock key
+        61,  # Lock with QR code success
+    ]
+
+    # 29-apply some force on the Lock
+    # 30-Door sensor closed
+    # 31-Door sensor open
+    # 32-open from inside
+    # 37-Remote Control
+    # 42-received new local mail
+    # 43-received new other cities' mail
+    # 44-Tamper alert
+    # 48-System locked ( Caused by, for example: Using INVALID Passcode/Fingerprint/Card several times)
+    # 51-Try to unlock with a deleted card
+    # 52-Dead lock with APP
+    # 53-Dead lock with passcode
+    # 54-The car left (for parking lock)
+    # 58-Unlock with QR code failed, it's expired
+    # 59-Double locked
+    # 60-Cancel double lock
+    # 62-Lock with QR code failed, the lock is double locked
+
     @property
     def extra_device_info(self) -> dict[str, Any]:
         """Lock specific device info."""
@@ -69,6 +114,24 @@ class TTLock(LockEntity, TTLockEntity[ApiLock]):
             ATTR_BATTERY_LEVEL: self.device.battery_level,
             VOLUME: self.device.volume if self.device.sound_enabled else 0,
         }
+
+    def update_from_webhook(self, data: dict):
+        """Process the webhook event to update lock state."""
+        if data.get("success") == 1:
+            # Only process lock state if the event indicated a successful operation
+            event = data.get("recordType", -1)
+            if event in self.RECORD_TYPE_UNLOCK:
+                self._attr_is_locked = False
+            elif event in self.RECORD_TYPE_LOCK:
+                self._attr_is_locked = True
+
+    @callback
+    def _update_callback(self, data: dict):
+        """Update data."""
+        if self.device.id == data.get("lockId"):
+            _LOGGER.debug(f"{data} is for this device ({self.entity_id})")
+            self.update_from_webhook(data)
+            self.async_write_ha_state()
 
     async def async_lock(self, **kwargs: Any) -> None:
         """Lock the car."""
