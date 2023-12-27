4a1b7711e6be75484f4d4fb043f7f97428f7463a
Jonas Bergler <jonas@bergler.name>
2023-12-27 20:30:38 +0000

    add a very basic test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,10 @@
 """Test ttlock setup process."""
 
+from unittest.mock import patch
+
 from custom_components.ttlock.const import DOMAIN
 from homeassistant.config_entries import ConfigEntryState
+from homeassistant.helpers.network import NoURLAvailableError
 
 
 async def test_setup_unload_and_reload_entry(hass, component_setup, sane_default_data):
@@ -15,3 +18,13 @@ async def test_setup_unload_and_reload_entry(hass, component_setup, sane_default
 
     assert await hass.config_entries.async_unload(entry.entry_id)
     assert entry.state == ConfigEntryState.NOT_LOADED
+
+
+@patch(
+    "homeassistant.components.webhook.async_generate_url",
+    side_effect=NoURLAvailableError,
+)
+async def test_no_url(hass, component_setup, sane_default_data):
+    with patch("homeassistant.helpers.issue_registry.async_create_issue") as mock:
+        assert await component_setup()
+        assert mock.assert_called
